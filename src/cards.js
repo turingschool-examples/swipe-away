4ab7cb00f3725350beb1e39db92971927132be46
Steve Kinney <hello@stevekinney.net>
2016-10-25 11:15:06 -0600

    Add event listeners

--- src/cards.js
+++ src/cards.js
@@ -1,6 +1,37 @@
 class Cards {
   constructor () {
     this.cards = document.querySelectorAll('.card');
+    this.addEventListeners();
+  }
+
+  addEventListeners () {
+    document.addEventListener('touchstart', e => this.onStart(e));
+    document.addEventListener('touchmove', e => this.onMove(e));
+    document.addEventListener('touchend', e => this.onEnd(e));
+
+    document.addEventListener('mousestart', e => this.onStart(e));
+    document.addEventListener('mousemove', e => this.onMove(e));
+    document.addEventListener('mouseend', e => this.onEnd(e));
+  }
+
+  onStart (event) {
+    if (!event.target.classList.contains('card')) return;
+
+    console.log(event.target);
+
+    event.preventDefault();
+  }
+
+  onMove (event) {
+
+  }
+
+  onEnd (event) {
+
+  }
+
+  update (event) {
+
   }
 }
 
