91b0ae45c37a0309a67b9af3e98ef8438a008ada
Steve Kinney <hello@stevekinney.net>
2016-10-25 11:34:20 -0600

    Get basic dragging working

--- src/cards.js
+++ src/cards.js
@@ -1,12 +1,20 @@
 class Cards {
   constructor () {
     this.cards = document.querySelectorAll('.card');
+
+    this.target = null;
+    this.startX = 0;
+    this.currentX = 0;
+
+    this.update = this.update.bind(this);
+
     this.addEventListeners();
+    requestAnimationFrame(this.update);
   }
 
   addEventListeners () {
     document.addEventListener('touchstart', e => this.onStart(e));
-    document.addEventListener('touchmove', e => this.onMove(e));
+    document.addEventListener('touchmove', e => this.onMove(e), { passive: true });
     document.addEventListener('touchend', e => this.onEnd(e));
 
     document.addEventListener('mousestart', e => this.onStart(e));
@@ -17,21 +25,31 @@ class Cards {
   onStart (event) {
     if (!event.target.classList.contains('card')) return;
 
-    console.log(event.target);
+    this.target = event.target;
+    this.startX = event.pageX || event.touches[0].pageX;
+    this.currentX = this.startX;
 
     event.preventDefault();
   }
 
   onMove (event) {
+    if (!this.target) return;
 
+    this.currentX = event.pageX || event.touches[0].pageX;
   }
 
   onEnd (event) {
-
+    if (!this.target) return;
   }
 
   update (event) {
+    requestAnimationFrame(this.update);
 
+    if (!this.target) return;
+
+    const screenX = this.currentX - this.startX;
+
+    this.target.style.transform = `translateX(${screenX}px)`;
   }
 }
 
